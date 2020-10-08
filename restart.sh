1709de996f626666be67191512e046b7683f00f4
khgoh99 <51729388+khgoh99@users.noreply.github.com>
2020-10-08 21:40:24 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 source config.source
 
-if [[ $TRAEFIKDOMAIN == "" ]];
-then
-        echo "**** TRAEFIKDOMAIN in config.source not set yet."
-	    echo "**** Please setup config.source first"
-	exit 1 
-else
-        echo "Domain is at $TRAEFIKDOMAIN"
-fi
+printf "\n"
+
+Check_Domain
+
+printf "********** Traefik Domain: $TRAEFIKDOMAIN \n"
+printf "********** Stop and start the Docker Service \n\n"
 
 if [ -z "$1" ]
 then
     # Run all the module listed in MODULELIST
+    printf "### Stopping all module ###\n" 
     for module in $MODULELIST
     do
-        (cd $module ;bash firsttime.sh)
+    	printf "### Module: $module --->\n"
+        (cd $module ;bash stop.bash; cd ..)
+	printf "<---\n\n"
     done
+    sleep 5
+    printf "### Starting all module ###\n" 
+    for module in $MODULELIST
+    do
+    	printf "### Module: $module --->\n"
+        (cd $module ;bash start.bash; cd ..)
+	printf "<---\n\n"
+    done    
 else 
     # Only run the given module
-    (cd $1 ;bash firsttime.sh)
+    printf "### Module: $1 --->\n"
+    (cd $1 ;bash stop.bash; cd ..)
+    printf "<---\n\n"
+    sleep 5
+    printf "### Module: $1 --->\n"
+    (cd $1 ;bash start.bash; cd ..)
+    printf "<---\n\n"    
 fi
