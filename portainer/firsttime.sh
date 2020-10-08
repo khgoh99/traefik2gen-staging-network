8b87691e9cc21ecaaba00ad0c22f00db5e642386
khgoh99 <51729388+khgoh99@users.noreply.github.com>
2020-10-08 14:45:14 +0800

    Update firsttime.sh

--- portainer/firsttime.sh
+++ portainer/firsttime.sh
@@ -2,23 +2,8 @@
 source ../config.source
 source config-custom.source
 
-if [[ $TRAEFIKDOMAIN == "" ]];
-then
-        echo "**** TRAEFIKDOMAIN in config.source not set yet."
-	      echo "**** Please setup config.source first"
-	exit 1 
-else
-        echo "Domain is at $TRAEFIKDOMAIN"
-fi
-
-echo "Creating the directory"
-mkdir -p $DATAPATH/$STACKNAME/setting
-mkdir -p $DATAPATH/$STACKNAME/log
-touch $DATAPATH/$STACKNAME/setting/acme.json
-chmod 600 $DATAPATH/$STACKNAME/setting/acme.json
-
+mkdir -p $DATAPATH/$STACKNAME/data
 chown -R nobody:nogroup $DATAPATH
 
 #creating the overlay network
-docker network create --driver=overlay traefik-public
 docker network create --driver=overlay agent-network
