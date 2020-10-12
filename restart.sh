46017fff3eb83b0ab53377c0b0326f2adb794b9c
khgoh99 <51729388+khgoh99@users.noreply.github.com>
2020-10-12 10:29:02 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source config.source
 
+WaitTime=8
+
 printf "\n"
 
 Check_Domain
@@ -8,7 +10,9 @@ Check_Domain
 printf "********** Traefik Domain: $TRAEFIKDOMAIN \n"
 printf "********** Stop and start the Docker Service \n\n"
 
-if [ -z "$1" ]
+arg=$1
+
+if [ -z "$arg" ]
 then
     # Run all the module listed in MODULELIST
     printf "### Stopping all module ###\n" 
@@ -18,7 +22,7 @@ then
         (cd $module ;bash stop.bash; cd ..)
 	printf "<---\n\n"
     done
-    sleep 5
+    Count_Down $WaitTime
     printf "### Starting all module ###\n" 
     for module in $MODULELIST
     do
@@ -28,11 +32,11 @@ then
     done    
 else 
     # Only run the given module
-    printf "### Module: $1 --->\n"
-    (cd $1 ;bash stop.bash; cd ..)
+    printf "### Module: $arg --->\n"
+    (cd $arg ;bash stop.bash; cd ..)
     printf "<---\n\n"
-    sleep 5
-    printf "### Module: $1 --->\n"
-    (cd $1 ;bash start.bash; cd ..)
+    Count_Down $WaitTime
+    printf "### Module: $arg --->\n"
+    (cd $arg ;bash start.bash; cd ..)
     printf "<---\n\n"    
 fi
