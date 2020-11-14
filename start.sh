44de7813f0d1b2baec79bf9825937e9a3d28ab47
root <root@staging.easyiot.link>
2020-11-14 13:49:56 +0800

    modified:   config.source 	modified:   start.sh 	modified:   stop.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
 source config.source
-
-printf "\n"
-if [[ $TRAEFIKDOMAIN == "" ]];
-then
-        echo "********** TRAEFIKDOMAIN in config.source not set yet."
-        echo "********** Please setup config.source first"
-	exit 1 
-fi
-
-printf "********** Traefik Domain: $TRAEFIKDOMAIN \n"
-printf "********** Starting the Docker services \n\n"
-
-
-if [ -z "$1" ]
-then
-    # Run all the module listed in MODULELIST
-    printf "### Processing all module ###\n" 
-    for module in $MODULELIST
-    do
-        printf "### Module: $module --->\n"
-        (cd $module ;bash start.bash; cd ..)
-	printf "<---\n\n"
-    done
-else 
-    # Only run the given module
-    printf "### Module: $1 --->\n"
-    (cd $1 ;bash start.bash; cd ..)
-    printf "<---\n\n"
-fi
+source $SCRIPTDIR/start.sh.root
--- stop.sh
+++ stop.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
 source config.source
+source $SCRIPTDIR/stop.sh.root
 
-printf "\n"
-if [[ $TRAEFIKDOMAIN == "" ]];
-then
-        echo "********** TRAEFIKDOMAIN in config.source not set yet."
-        echo "********** Please setup config.source first"
-	exit 1 
-fi
-
-printf "********** Traefik Domain: $TRAEFIKDOMAIN \n"
-printf "********** Stopping the Docker Services \n\n"
-
-
-if [ -z "$1" ]
-then
-    # Run all the module listed in MODULELIST
-    printf "### Processing all module ###\n" 
-    for module in $MODULELIST
-    do
-        printf "### Module: $module --->\n"
-        (cd $module;bash stop.bash; cd ..)
-	printf "<---\n\n"
-    done
-else 
-    # Only run the given module
-    printf "### Module: $1 --->\n"
-    (cd $1;bash stop.bash; cd ..)
-    printf "<---\n\n"
-fi
