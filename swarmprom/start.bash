3b973e891e2487e29639fde7d14b54eb320f4770
root <root@staging.easyiot.link>
2020-11-14 12:54:02 +0800

    deleted:    swarmprom/compose-custom.yml 	deleted:    swarmprom/config-custom.source 	deleted:    swarmprom/config/alertmanager/alertmanager.yml 	deleted:    swarmprom/config/caddy/Caddyfile 	deleted:    swarmprom/config/grafana/dashboards/swarmprom-nodes-dash.json 	deleted:    swarmprom/config/grafana/dashboards/swarmprom-prometheus-dash.json 	deleted:    swarmprom/config/grafana/dashboards/swarmprom-services-dash.json 	deleted:    swarmprom/config/grafana/grafana.ini 	deleted:    swarmprom/config/grafana/provisioning/dashboards/swarmprom_dashboards.yml 	deleted:    swarmprom/config/grafana/provisioning/datasources/prometheus.yaml 	deleted:    swarmprom/config/prometheus/prometheus.yml 	deleted:    swarmprom/firsttime.bash 	deleted:    swarmprom/start.bash 	deleted:    swarmprom/stop.bash

--- swarmprom/start.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-source ../config.source
-source $SCRIPTDIR/start_script.bash
-
-#source config-custom.source
-
-#echo "#### Starting/Updating ${DISTROSTACK}"
-#docker stack deploy -c ${BASESTACKYML} -c compose-custom.yml ${DISTROSTACK}
--- swarmprom/stop.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-source ../config.source
-source $SCRIPTDIR/stop_script.bash
